a67daf31fc05165239e6c65161a30000367c1985
Huang <Ellen.Huang@ingrammicro.com>
2019-11-01 07:47:29 +0800

    frant page

---
+++ src/components/pages/ShoppingMall.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <!--search bar layout-->
+    <div class="search-bar">
+      <van-row gutter="5">
+        <van-col span="3">
+          <img :src="locationIcon" width="80%" class="location-icon" />
+        </van-col>
+        <van-col span="16">
+          <input type="text" class="search-input" />
+        </van-col>
+        <van-col span="5">
+          <van-button size="mini">查找</van-button>
+        </van-col>
+      </van-row>
+    </div>
+    <!--swipwer area-->
+    <div class="swiper-area">
+      <van-swipe :autoplay="1000">
+        <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
+          <img v-lazy="banner.imageUrl" width="100%" />
+        </van-swipe-item>
+      </van-swipe>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      locationIcon: require("../../assets/images/location.png"),
+      bannerPicArray: [
+        {
+          imageUrl: require("../../assets/images/swipe/a1.jpg")
+        },
+        {
+          imageUrl: require("../../assets/images/swipe/a2.jpg")
+        },
+        {
+          imageUrl: require("../../assets/images/swipe/a3.jpeg")
+        }
+      ]
+    };
+  },
+  created() {
+    axios({
+      url:
+        "https://www.easy-mock.com/mock/5dbb28c5727c0077ea997ce9/example/ShoppingMall/home",
+      method: "get"
+    })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {});
+  }
+};
+</script>
+
+<style scoped>
+.search-bar {
+  height: 2.2rem;
+  background-color: #e5017d;
+  line-height: 2.2rem;
+}
+.search-input {
+  width: 100%;
+  height: 1.3rem;
+  border-top: 0px;
+  border-left: 0px;
+  border-right: 0px;
+  border-bottom: 1px solid 1px !important ;
+  background-color: #e5017d;
+  color: #fff;
+}
+.location-icon {
+  padding-top: 0.2rem;
+  padding-left: 0.3rem;
+}
+
+.swiper-area {
+  width: 20rem;
+  clear: both;
+  max-height: 8rem;
+  overflow: hidden;
+}
+</style>
